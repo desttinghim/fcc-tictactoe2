6c2e0c2e9930e09734e4495216126e1fa6089c3e
Louis Pearson <desttinghimgame@gmail.com>
2017-03-27 17:08:09 -0500

    Add choose and multiple piece placing

--- src/Main.hx
+++ src/Main.hx
@@ -14,27 +14,47 @@ class Main {
 
 class TicTacToe implements Mithril {
 
+    public var currentPiece : Piece;
     var board : Array<Place>;
+    var choose : Choose;
 
     public function new() {
-        this.board = [for (i in 0...9) new Place(E)];
+        this.choose = new Choose(this);
+        this.board = [for (i in 0...9) new Place(this, E)];
+        currentPiece = E;
     }
 
     public function view() [
-        m('.tictactoe#app', [for (i in board) m(i)]),
+        m('#app',
+            currentPiece == E ?
+                m(choose)
+                : m('.tictactoe', [for (i in board) m(i)])),
     ];
 
 }
 
+class Choose implements Mithril {
+    var t : TicTacToe;
+    public function new(t) {this.t = t;}
+    public function view() [
+        m('.choose', [
+            m('.piece.$X', {onclick: function() t.currentPiece = X}),
+            m('.piece.$O', {onclick: function() t.currentPiece = O}),])
+    ];
+}
+
 class Place implements Mithril{
+    var t : TicTacToe;
     var piece : Piece;
-    public function new(piece) {
+    public function new(t, piece) {
+        this.t = t;
         this.piece = piece;
     }
     public function view() [
         m('.piece.$piece', {onclick: change}),
     ];
     public function change() {
-        piece = X;
+        piece = t.currentPiece;
+        t.currentPiece = t.currentPiece == X ? O : X;
     }
 }
